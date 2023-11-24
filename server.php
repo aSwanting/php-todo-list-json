3c233c31f72059b6fc50820aa7a25058e58df727
aSwanting <gab.damico@gmail.com>
2023-11-24 16:45:43 +0100

    add: comments

--- server.php
+++ server.php
@@ -1,12 +1,14 @@
 <?php
-
+// Get and decode Json object
 $todolist_json = file_get_contents('./todolist.json');
 $todo_decode = json_decode($todolist_json, true);
 
+// Store contents in response
 $response = [
     "success" => true,
     "results" => $todo_decode
 ];
 
+// Respond
 header("Content-type: application/json");
 echo json_encode($response);
